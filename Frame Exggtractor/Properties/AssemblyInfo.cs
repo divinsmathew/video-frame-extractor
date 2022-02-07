c8fd0509a87f5dfd30fda8409d2f8b3f0a241d0b
Divins Mathew <divinsmathew@gmail.com>
2022-02-08 00:03:29 +0530

    Renamed Solution

--- Frame Exggtractor/Properties/AssemblyInfo.cs
+++ Frame Exggtractor/Properties/AssemblyInfo.cs
@@ -1,15 +1,14 @@
 ﻿using System.Reflection;
-using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Motta's Frame Eggxtractor")]
+[assembly: AssemblyTitle("Frame Eggxtractor")]
 [assembly: AssemblyDescription("diozz")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("diozz")]
-[assembly: AssemblyProduct("Motta's Frame Eggxtractor")]
+[assembly: AssemblyProduct("Frame Eggxtractor")]
 [assembly: AssemblyCopyright("Copyright ©  2019")]
 [assembly: AssemblyTrademark("diozz")]
 [assembly: AssemblyCulture("")]
